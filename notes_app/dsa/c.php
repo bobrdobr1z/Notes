573474cde301ddc5a1bd191b59b836c8dfb71d77
bobrdobr1z <bobrdobr112zxc@bk.ru>
2025-04-15 21:21:15 +0300

    моя игра в сапера

---
+++ notes_app/dsa/c.php
@@ -0,0 +1,25 @@
+<?php
+$dbName = 'novels.db';
+
+try {
+    $db = new PDO("sqlite:$dbName");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $createTableQuery = "
+    CREATE TABLE IF NOT EXISTS novels (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,  
+        title TEXT NOT NULL,
+        author TEXT NOT NULL,
+        url TEXT NOT NULL UNIQUE,
+        last_update DATETIME DEFAULT CURRENT_TIMESTAMP  
+    );";
+
+    $db->exec($createTableQuery);
+    echo "База данных и таблица успешно созданы!";    
+} catch (PDOException $e) {
+    echo "Ошибка: " . $e->getMessage();
+}
+
+$db = null;
+
+?>
